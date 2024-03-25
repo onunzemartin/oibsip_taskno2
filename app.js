1c2307d7ac39707594a496db89595a567223521e
onunzemartin <martinonunzekk@gmail.com>
2024-03-25 13:11:37 +0100

    added new files

---
+++ app.js
@@ -0,0 +1,13 @@
+// Initialize Swiper  when page loads
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
